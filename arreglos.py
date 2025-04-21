450501220a9540d2edb543d873abf163345f82e9
Rena41615 <rena41615@gmail.comm>
2025-04-21 13:29:58 -0400

    avances de clase de arreglos

--- arreglos.py
+++ arreglos.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+Estudiantes = [
+    {"nombre": "Renata" , "edad": 18 , "carrera": "Ingeniería en Informática"},
+    {"nombre": "Fabita" , "edad": 18 , "carrera": "Odontología"},
+]
+option = input("¿Qué nombre de estudiante quieres ver? (0 o 1): ")
+if option == "0":
+    print(Estudiantes[0]["nombre"])
+else:
+    print(Estudiantes[1]["nombre"])
